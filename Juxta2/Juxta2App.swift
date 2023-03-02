479500b77a674d2985e839be0ceeee4cf85f2186
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-03-02 15:02:04 -0500

    Initial Commit

---
+++ Juxta2/Juxta2App.swift
@@ -0,0 +1,17 @@
+//
+//  Juxta2App.swift
+//  Juxta2
+//
+//  Created by Matt Gaidica on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Juxta2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
